d41f51092abc0378c78352381858a9e74d9b7ee3
Rixbroho <ratnaprajapati10@gmail.com>
2025-01-10 19:08:31 +0545

    tic tac toe with some feature

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const boxes=document.querySelectorAll(".box");
+const xCount=document.getElementById('xCount');
+const oCount=document.getElementById('oCount');
+const btns=document.querySelectorAll('section>button')
 
 const win=[
     [0,1,2],
@@ -12,6 +15,9 @@ const win=[
 ]
 
 let Xturn=true;
+let count=0;
+let xWin=0;
+let oWin=0;
 
 boxes.forEach((box)=>{
     box.addEventListener('click',()=>{
@@ -23,15 +29,26 @@ boxes.forEach((box)=>{
             box.innerHTML='O';
         }
         box.disabled=true;
-        // box.apple=true;
-        Xturn=!Xturn;
+        // box.apple=true; this will not work
+        turnCheck();
+        count++;
         checkWin();
     })
 })
 
+const turnCheck=()=>{
+    if(Xturn){
+        document.getElementById('turn').innerHTML='O';
+    }
+    else{
+        document.getElementById('turn').innerHTML='X';
+    }
+    Xturn=!Xturn;
+}
+
 const checkWin = ()=>{
     for(patten of win){
-        console.log(patten[0],patten[1],patten[2]);
+        // console.log(patten[0],patten[1],patten[2]);
         // console.log(boxes[patten[0]],boxes[patten[1]],boxes[patten[2]]);
         let var1=boxes[patten[0]].innerHTML;
         let var2=boxes[patten[1]].innerHTML;
@@ -39,20 +56,41 @@ const checkWin = ()=>{
         // console.log(var1);
         // console.log(var2);
         // console.log(var3);
-        if(var1!='' && var2!=''){
+        if(var1!='' && var2!='' && var3!=''){
             if(var1==var2 && var2==var3){
                 if(var1=='X'){
                     alert('X won');
+                    xWin++;
+                    xCount.innerText=xWin;
                 }
-                else{
+                else if (var1=='O'){
                     alert('O won');
+                    oWin++;
+                    oCount.innerText=oWin;
                 }
-                reset();
+                boxes.forEach((box)=>{
+                    box.disabled=true;
+                })
+                count=0;
+                return;
+                // reset();
             }
         }
     }
+    if(count==9){
+        console.log('done')
+        count=0;
+        alert('Draw');
+    }
 }
 
+
+function nextGame(){
+    boxes.forEach((box)=>{
+        box.disabled=false;
+        box.innerHTML='';
+    })
+}
 
 //to reload the pagae
 function reset(){
